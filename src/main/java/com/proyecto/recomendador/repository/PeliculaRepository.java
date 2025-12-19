1f78c6363a8af588e6cc6ba00b49d79937bec9f5
csantillgar <146821944+csantillgar@users.noreply.github.com>
2025-12-19 13:29:23 +0100

    Delete src directory

--- src/main/java/com/proyecto/recomendador/repository/PeliculaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.proyecto.recomendador.repository;
-import com.proyecto.recomendador.model.Pelicula;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PeliculaRepository extends JpaRepository<Pelicula, Long> {
-}
--- src/main/java/com/proyecto/recomendador/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.proyecto.recomendador.repository;
-import com.proyecto.recomendador.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByCorreo(String correo);
-}
